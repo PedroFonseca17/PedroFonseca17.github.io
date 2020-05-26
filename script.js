679d7e3f6a43e20c8300acc2265dd906b54d6808
Pedro Fonseca <pedro.h.fonseca17@gmail.com>
2020-05-26 16:31:45 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+<script>
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+  console.log('came from github');
+</script>
